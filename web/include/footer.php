ec2add765c0e4ba1e658c074bba3c99202e75412
Emiliano Gioia <emilianogioia.dg@gmail.com>
2019-11-19 01:18:56 -0300

    Estructura Carpetas + Index Maquetado en Bootstrap (Pelado) + Includes

---
+++ web/include/footer.php
@@ -0,0 +1,35 @@
+<footer class="container-fluid">
+        <div class="row">
+            <div class="container">
+                <div class="row">
+                    <div class="col-4">
+                        <h4>Nosotros</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sollicitudin, justo sed tempor dapibus, diam mi scelerisque neque, vitae sodales mauris nibh eget risus. Nulla facilisi. Aenean sit amet felis eu tortor sollicitudin tincidunt. Curabitur porta elit at enim gravida, in aliquet tellus dignissim. Fusce tristique mattis augue, in efficitur metus imperdiet nec. Donec id facilisis arcu. Aliquam congue, dolor nec hendrerit tristique, nunc felis viverra lectus, quis fermentum sem eros nec orci. Mauris vel semper nisl. Nulla imperdiet auctor nulla pharetra rutrum. Aenean sit amet sem non felis varius imperdiet non et quam.</p>
+                    </div>
+
+                    <div class="col-4">
+                        <a href="#">Arte y Diseño</a>
+                        <a href="#">Programación y Lógica</a>
+                        <a href="#">Fotografía</a>
+                        <a href="#">Moda </a>
+                        <a href="#">Storytelling</a>
+                    </div>
+
+                    <div class="col-4">
+                        <a href="#">Home</a>
+                        <a href="#">Iniciar Sesión / Registrarse</a>
+                        <a href="#">Preguntas Frecuentes</a>
+                        <a href="#">Contacto</a>
+                        <a href="#">Política de Privacidad</a>
+                    </div>    
+                </div>
+    
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-12">
+                <p>Todos los derechos reservados</p>
+            </div>
+        </div>
+</footer>
---
+++ web/index.php
@@ -0,0 +1,271 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+$title="Inicio";
+include("include/head.php");
+?>
+
+<body>
+    <?php include('include/header.php');?>
+    <section class="container-fluid">
+        <div class="row">
+            <div class="col-12 col-md-8">
+                <h1>Superá tus límites participando en la primera red social de desafíos digitales</h1>
+            </div>
+
+            <div class="col-12 col-md-4">
+                <img src="img/logo.svg" alt="">
+                <form>
+
+                <div class="form-group">
+                    <label for="inputName">Tu nombre y Apellido</label>
+                    <input type="text" class="form-control" id="inputName">
+                </div>
+
+                <div class="form-group">
+                    <label for="inputMail">Tu mail</label>
+                    <input type="email" class="form-control" id="inputMail" >
+                </div>
+
+
+                <button type="submit" class="btn btn-primary">Avanzar</button>
+                </form>                
+            </div>
+
+            <div class="col-3">
+                <img src="" alt="">
+                <p>@mark78 - Diseñador Web</p>
+            </div>
+
+        </div> <!-- CIERRE DE ROW HERO -->
+    </section> <!-- CIERRE DEL HERO -->
+
+    <section class="container">
+        <div class="row">
+            <div class="col-12">
+                <h2>Chally es muy simple.</h2>
+            </div>
+
+            <div class="col-4">
+                <img src="" alt="">
+                <p>Paso 1</p>
+                <p>Buscás desafíos de tu área de interés. ¡También podés crear tu propio desafío!</p>
+            </div>
+
+            <div class="col-4">
+                <img src="" alt="">
+                <p>Paso 2</p>
+                <p>Participás enviando tu respuesta y competís con miles de challengers en todo el mundo</p>
+            </div>
+
+            <div class="col-4">
+                <img src="" alt="">
+                <p>Paso 1</p>
+                <p>Además de ejercitar el cerebro y compartir conocimiento, Chally arma tu portfolio profesional en base a tus respuestas.</p>
+            </div>
+
+        
+        </div>
+    </section> <!-- CIERRE SECCION ACERCA DE CHALLY STEPS -->
+
+    <section class="container">
+        <div class="row">
+            <div class="col-12">
+                <h2>Tenemos desafíos para todos los gustos.</h2>
+            </div>
+
+            <div class="col-12">
+                <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <div class="row">
+                                <div class="col-4">
+                                    <img src="" alt="">
+                                    <h3>Diseño y Arte</h3>
+                                    <p>4258 Desafíos Abiertos</p>
+                                    <a href="#">Ver Desafíos Destacados</a>
+                                </div>
+
+                                <div class="col-4">
+                                    <img src="" alt="">
+                                    <h3>Diseño y Arte</h3>
+                                    <p>4258 Desafíos Abiertos</p>
+                                    <a href="#">Ver Desafíos Destacados</a>
+                                </div>
+
+                                <div class="col-4">
+                                    <img src="" alt="">
+                                    <h3>Diseño y Arte</h3>
+                                    <p>4258 Desafíos Abiertos</p>
+                                    <a href="#">Ver Desafíos Destacados</a>
+                                </div>
+
+                            </div>
+                        </div>
+
+                        <div class="carousel-item">
+                            <div class="row">
+                                <div class="col-4">
+                                    <img src="" alt="">
+                                    <h3>Diseño y Arte</h3>
+                                    <p>4258 Desafíos Abiertos</p>
+                                    <a href="#">Ver Desafíos Destacados</a>
+                                </div>
+
+                                <div class="col-4">
+                                    <img src="" alt="">
+                                    <h3>Diseño y Arte</h3>
+                                    <p>4258 Desafíos Abiertos</p>
+                                    <a href="#">Ver Desafíos Destacados</a>
+                                </div>
+
+                                <div class="col-4">
+                                    <img src="" alt="">
+                                    <h3>Diseño y Arte</h3>
+                                    <p>4258 Desafíos Abiertos</p>
+                                    <a href="#">Ver Desafíos Destacados</a>
+                                </div>
+
+                            </div>
+                        </div>
+
+
+                    </div>
+                    <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
+                </div>  <!-- CIERRE CARRUSEL -->            
+            </div>
+            
+        </div> <!-- CIERRE ROW  CATEGORIAS --> 
+    </section>  <!-- CIERRE CONTENEDOR SECCION CATEGORIAS -->  
+
+    <section class="container">
+        <div class="row">
+            <div class="col-12">
+                <h2>Challys destacados de la Semana</h2>
+            </div>
+
+            <div class="col-3">
+                <img src="" alt="">
+                <p>Ejemplo de desafío que por el momento no se me ocurre </p>
+                <p>¡985 challengers participando!</p>
+            </div>
+
+            <div class="col-3">
+                <img src="" alt="">
+                <p>Ejemplo de desafío que por el momento no se me ocurre </p>
+                <p>¡985 challengers participando!</p>
+            </div>
+
+            <div class="col-3">
+                <img src="" alt="">
+                <p>Ejemplo de desafío que por el momento no se me ocurre </p>
+                <p>¡985 challengers participando!</p>
+            </div>
+
+            <div class="col-3">
+                <img src="" alt="">
+                <p>Ejemplo de desafío que por el momento no se me ocurre </p>
+                <p>¡985 challengers participando!</p>
+            </div>
+
+            <div class="col-12">
+                <h2>Challengers Destacados del Mes</h2>
+            </div>
+
+            <div class="col-3">
+                <img src="" alt="">
+                <p>Rogelio Buendia <br> @rogelio48</p>
+                <a href="#">Seguir</a>
+            </div>
+
+            <div class="col-3">
+                <img src="" alt="">
+                <p>Rogelio Buendia <br> @rogelio48</p>
+                <a href="#">Seguir</a>
+            </div>
+
+            <div class="col-3">
+                <img src="" alt="">
+                <p>Rogelio Buendia <br> @rogelio48</p>
+                <a href="#">Seguir</a>
+            </div>
+
+            <div class="col-3">
+                <img src="" alt="">
+                <p>Rogelio Buendia <br> @rogelio48</p>
+                <a href="#">Seguir</a>
+            </div>
+
+
+        </div>
+    </section> <!-- CIERRE SECCION DE DESTACADOS -->
+
+
+    <section class="container-fluid">
+        <div class="row">
+            <div class="col-4">
+            <div class="accordion" id="accordionExample">
+                <div class="card">
+                    <div class="card-header" id="headingOne">
+                    <h2 class="mb-0">
+                        <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                        Collapsible Group Item #1
+                        </button>
+                    </h2>
+                    </div>
+
+                    <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
+                    <div class="card-body">
+                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                    </div>
+                    </div>
+                </div>
+
+
+                <div class="card">
+                    <div class="card-header" id="headingTwo">
+                    <h2 class="mb-0">
+                        <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                        Collapsible Group Item #2
+                        </button>
+                    </h2>
+                    </div>
+                    <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+                    <div class="card-body">
+                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                    </div>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div class="card-header" id="headingThree">
+                    <h2 class="mb-0">
+                        <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                        Collapsible Group Item #3
+                        </button>
+                    </h2>
+                    </div>
+                    <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+                    <div class="card-body">
+                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                    </div>
+                    </div>
+                </div>
+            </div>
+            </div>
+        </div>
+    </section> <!-- CIERRE SECCION PREGUNTAS -->
+
+    <?php include("include/footer.php");?>
+
+</body>
+
+
+</html>
