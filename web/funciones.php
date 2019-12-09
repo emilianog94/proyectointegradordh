6f0e1e39d2231f622b1c35c576fd2c7ec71e005c
FrancoFoumantin <francofourmantin@gmail.com>
2019-12-09 17:29:01 -0300

    Se subio archivo de funciones, y el registro ya registra, todavia falta mostrar errores y persistir datos

---
+++ web/funciones.php
@@ -0,0 +1,212 @@
+<?php
+
+
+//FUNCION PRE PARA LOS VARDUMP LINDOS 
+function pre($dato){
+  echo "<pre>";
+  var_dump($dato);
+  echo "</pre>";
+}
+
+
+//DEFINO VALORES DE KB MB GB Y TB PARA HACER VALIDACIONES DE TAMAÑO DE IMAGEN
+
+define('KB', 1024);
+define('MB', 1048576);
+define('GB', 1073741824);
+define('TB', 1099511627776);
+
+
+
+// FUNCION PARA ABRIR JSON Y DEVOLVER UN ARRAY DECODEADO (NECESITA LA RUTA DEL JSON)
+function abrirJson($dirJson){
+  $json = file_get_contents($dirJson);
+  $arrayUsuarios = json_decode($json , true);
+  return $arrayUsuarios;
+}
+
+
+//FUNCION PARA MODIFICAR EL ARCHIVO JSON, NECESITA EL NUEVO DATO , EL CAMPO QUE SE VA A MODIFICAR, EL DATO VIEJO PARA PODER REEMPLARLO Y UN ID PARA UNA VERIFICACION.
+
+function modificarJson($nuevo_dato , $campo_para_modificar, $dato_viejo  , $id){
+  $arrayDeUsuarios = abrirJson("usuarios.json");
+  foreach($arrayDeUsuarios as $usuarios){
+    if ($usuarios["$campo_para_modificar"] == $dato_viejo && $usuarios['id'] == $_SESSION['id']){
+      $usuarios["$campo_para_modificar"] = $nuevo_dato;
+    }
+    $arrayDeUsuariosFinal[] = $usuarios;
+  }
+
+  $json = json_encode($arrayDeUsuariosFinal);
+  file_put_contents('usuarios.json' , $json); 
+}
+
+
+
+// FUNCIÓN PARA VALIDAR CAMPOS DEL FORMULARIO DE REGISTRO
+function validar($data) {
+
+    // Creo un array de errores vacío.
+    $errores = [];
+
+    // CAMPO NOMBRE
+    $nombre = trim($data['name']);
+    // si está vacío,
+    if($nombre == "") {
+      // creo la posición "name" en el array de errores y guardo el string con el error que le quiero mostrar al usuario
+      $errores['name'] = "El nombre es obligatorio";
+    }
+
+    // CAMPO EMAIL
+    $email = trim($data['email']);
+    $email = filter_var($email, FILTER_SANITIZE_EMAIL);
+    // si está vacío
+    if($email == ""){
+      // creo la posición "email" en el array de errores y guardo el string con el error que le quiero mostrar al usuario
+      $errores['email'] = "El mail es obligatorio";
+    } elseif(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      $errores['email'] = "El email ingresado no es válido";
+    } else {
+
+      //ABRO EL JSON PARA CHEQUEAR QUE NO EXISTA OTRO MAIL IGUAL EN LA BASE DE DATOS
+      $directorioJson = "usuarios.json";
+      $todosLosUsuarios = abrirJson($directorioJson);
+      foreach($todosLosUsuarios as $usuarios){
+        if($usuarios['email'] == $email){
+          $errores['email'] = "El mail ya existe";
+        }
+      }
+
+      //AQUI SE HACE LA VALIDACION CON LA CONFIRMACION DE MAIL
+      if(isset($_POST['validacion_email'])){
+        if(strlen($_POST['validacion_email'])== 0){
+            $errores['validacion_email'] = "Este campo no se puede encontrar vacio";
+        }elseif (!($_POST['validacion_email'] == $_POST['email'])){
+            $errores['validacion_email'] = "El mail no coincide con el ingresado";
+        }else{
+            $validado['validacion_email'] = $_POST['validacion_email'];
+        }
+    }
+    }
+    
+    // CAMPO NOMBRE DE USUARIO
+    $userName = trim($data['username']);
+    if(strlen($userName) < 5) {
+      // creo la posición "username" en el array de errores y guardo el string con el error que le quiero mostrar al usuario
+      $errores['username'] = "El nombre de usuario debe tener al menos 5 caracteres";
+    }else {
+
+        //AQUI SE ABRE EL JSON PARA CHEQUEAR QUE NO EXISTA OTRO USERNAME EN LA BASE
+        $directorioJson = "usuarios.json";
+        $todosLosUsuarios = abrirJson($directorioJson);
+        foreach($todosLosUsuarios as $usuarios){
+          if($usuarios['username'] == $userName){
+            $errores['username'] = "El username ya existe";
+          }
+        }
+    }
+    
+    // CAMPO AVATAR
+    $avatar = $_FILES['avatar'];
+    // si no cargaron ningún archivo
+    if($avatar['error']) {
+      // creo la posición "avatar" en el array de errores y guardo el string con el error que le quiero mostrar al usuario
+      $errores['avatar'] = "Debe subir una foto de perfil";
+    } else {
+        // obtengo la extensión
+        $ext = pathinfo($avatar['name'], PATHINFO_EXTENSION);
+        if($ext !== 'jpg' && $ext !== 'jpeg' && $ext !== 'png') {
+          $errores['avatar'] = "La extensión del archivo debe ser jpg, png ó jpeg";
+        }elseif ($avatar['size'] > 2*MB){
+            $errores['avatar'] == "El archivo es demasiado pesado, maximo 2MB";
+        }
+
+     }
+
+
+    // CAMPO CONTRASEÑA
+    $password = trim($data['password']);
+    // si está vacío
+    if($password == "" ) {
+      // creo la posición "password" en el array de errores y guardo el string con el error que le quiero mostrar al usuario
+      $errores['password'] = "La contraseña es obligatoria";
+    }else{
+      $uppercase = preg_match('@[A-Z]@', $_POST['password']);
+      $lowercase = preg_match('@[a-z]@', $_POST['password']);
+      $number    = preg_match('@[0-9]@', $_POST['password']);
+      $specialChars = preg_match('@[^\w]@', $_POST['password']);
+      // si tiene una longitud menos a 6
+      if(!$uppercase || !$lowercase || !$number || !$specialChars || strlen($_POST['password']) < 8) {
+      // creo la posición "password" en el array de errores y guardo el string con el error que le quiero mostrar al usuario
+      $errores['password'] = "La contraseña debe tener al menos 8 caracteres de longitud y debe incluir al menos una letra mayúscula, un número y un carácter especial";
+      }
+    }
+
+    
+    // CAMPO REPETIR CONTRASEÑA
+    $cpassword = trim($data['confirm-password']);
+    // si está vacío
+    if($cpassword == "") {
+      // creo la posición "repassword" en el array de errores y guardo el string con el error que le quiero mostrar al usuario
+      $errores['repassword'] = "Debe repetir la contraseña para continuar";
+    } 
+      // si es diferente al password que escribió
+      elseif($cpassword != $password) {
+      // creo la posición "repassword" en el array de errores y guardo el string con el error que le quiero mostrar al usuario
+      $errores['repassword'] = "Las contraseñas no coinciden";
+    }
+
+    // devuelvo el array de errores. Si no entró en ninǵun condicional de los declarados arriba, el array de errores va a estar vacío
+    return $errores;
+  }
+
+
+// FUNCIÓN PARA GUARGAR IMAGEN
+
+function guardarAvatar() {
+    // me guardo la extensión del archivo
+    $ext = pathinfo($_FILES['avatar']['name'], PATHINFO_EXTENSION);
+
+    // me guardo la carpeta temporal en la que se encuentra
+    $directorioTemporal = $_FILES['avatar']['tmp_name'];
+
+    // armo el nombre con el que voy a guardar la imagen. La función uniqid() puede recibir un string, que será el prefijo del id aleatorio generado
+    $nombreImagen = uniqid('img_') . '.' . $ext;
+    
+    // armo la ruta final de la imagen, concatenando al final el nombre que creé
+    $carpetaFinal = dirname(__FILE__) . '/avatars/' . $nombreImagen;
+    
+    // muevo el archivo a la carpeta avatars
+    move_uploaded_file($directorioTemporal, $carpetaFinal);
+    
+    // devuelvo el nombre de la imagen que armé, para guardarlo en el array del usuario
+    return $nombreImagen;
+}
+
+
+
+
+//FUNCIÓN PARA CREAR UN ARRAY ASOCIATIVO CON LOS DATOS QUE ME LLEGAN POR POST
+
+function guardarUsuario($data) {
+    $dirJson = "usuarios.json";
+
+    $todosLosUsuarios = abrirJson($dirJson);
+    $totalUsuarios = count($todosLosUsuarios);
+    
+    $data['id'] = $totalUsuarios;
+    
+    
+    $usuario = [
+        "id" => $data['id'],
+        "name" => $data["name"],
+        "lastname" => $data['username'],
+        "email" => $data["email"],
+        "birth" => $data['birth'],
+        "username" => $data["username"],
+        "password" => password_hash($data["password"], PASSWORD_DEFAULT),
+    ];
+    return $usuario;
+}
+
+?>
--- web/registro.php
+++ web/registro.php
@@ -1,163 +1,212 @@
-<!DOCTYPE html>
-<html lang="en">
 <?php
-$title="Registro";
-include("include/head.php");
-?>
 
 
-<body class="animated fadeIn">
-    <div class="contenedor-registro">
+require('funciones.php');
+
+if($_POST){
+
+    $errores = [];
+
+    pre($_POST);
+    
+    if(!$errores) {
+        // llamo a la función guardarUsuario() --> me devuelve un array asociativo con los datos que envió el usuario
+        $usuario = guardarUsuario($_POST);
         
-        <section class="container-fluid px-5">
-            <div class="row d-flex align-items-center justify-content-center vh-100 flex-wrap">
-                
+        // llamo a la función guardarAvatar() --> guarda la imagen y devuelve le nombre con el que guardé la imagen
+        $nombreImagen = guardarAvatar();
+        
+        // al array asocativo del nuevo usuario, le creo la posición "avatar" para guardar el nombre de la imagen que subió el usuario
+        $usuario['avatar'] = $nombreImagen;
+        
+        // me traigo el contenido del archivo usuarios.json
+        $listaDeUsuarios = file_get_contents('usuarios.json');
+        
+        // convierto ese contenido a formato array para poder manipular los datos
+        $arrayUsuarios = json_decode($listaDeUsuarios, true);
+        
+        // en la última posicón del array de usuarios me guardo al nuevo usuario
+        $arrayUsuarios[] = $usuario;
+        
+        // convierto el aray de usuarios a formato json para volver a guardarlo en el archivo de usuarios
+        $todosLosUsuarios = json_encode($arrayUsuarios);
+        
+        // guardo el json completo de ususarios en usuarios.json 
+        file_put_contents('usuarios.json', $todosLosUsuarios);
+
+        }
+    }
+    
+    
+    ?>
+    
+    
+    
+    
+    
+    
+    <!DOCTYPE html>
+    <html lang="en">
+
+    <?php
+    $title="Registro";
+    include("include/head.php");
+    ?>
+    
+    <body class="animated fadeIn">
+        <div class="contenedor-registro">
+            
+            <section class="container-fluid px-5">
+                <div class="row d-flex align-items-center justify-content-center vh-100 flex-wrap">
+                    
                     
                     
                     <div class="col-12 col-sm-12 col-md-8 col-lg-5 shadow contacto-form px-5 py-3 d-flex flex-column align-items-center my-5">
                         <a href="index.php"><img  src="img/logo_c.svg" class="logo" alt=""></a>
                         <h3 class="color-verde text-center mb-5 ">Registro</h3>
-                        <form class="w-100 needs-validation" method="POST" action="register.php">
+                        <form class="w-100 needs-validation" method="POST" action="registro.php" enctype="multipart/form-data">
                             
                             <div class="form-row">
                                 
                                 <div class="col-12 col-sm-12 col-md-6 col-lg-6 mb-0 mb-md-4 ">
                                     <div class="form-group">
                                         <label for="inputName">Tu nombre</label>
-                                        <input type="text" class="form-control" id="inputName" required>
+                                        <input type="text" class="form-control" name="name" required>
                                     </div>
                                 </div>
                                 
                                 <div class="col-12 col-sm-12 col-md-6 col-lg-6  mb-0 mb-md-4 ">
                                     <div class="form-group">
                                         <label for="inputName">Tu Apellido</label>
-                                        <input type="text" class="form-control" id="inputName" required>
+                                        <input type="text" class="form-control" name="lastname" required>
                                     </div>
                                 </div>
                                 
                                 <div class="col-12 col-sm-12 col-md-12 col-lg-12  mb-0 mb-md-4 ">
                                     <div class="form-group">
                                         <label for="inputName">Nombre de usuario</label>
-                                        <input type="text" class="form-control " id="inputName" required>
+                                        <input type="text" class="form-control" name="username" required>
                                     </div>
                                 </div>
                                 
                                 <div class="col-12 col-sm-12 col-md-6 col-lg-6  mb-0 mb-md-4 ">
                                     <div class="form-group">
                                         <label for="inputMail">Tu mail</label>
-                                        <input type="email" class="form-control " id="inputMail" required>
+                                        <input type="email" class="form-control " name="email" required>
                                     </div>
                                 </div>
                                 
                                 <div class="col-12 col-sm-12 col-md-6 col-lg-6   mb-0 mb-md-4">
                                     <div class="form-group">
                                         <label for="inputMail">Confirmacion mail</label>
-                                        <input type="email" class="form-control " id="inputMail" required>
+                                        <input type="email" class="form-control " name="validacion_email" required>
                                     </div>
                                 </div>
                                 
                                 <div class="col-12 col-sm-12 col-md-6 col-lg-6  mb-0 mb-md-4 ">
                                     <div class="form-group">
                                         <label for="inputPassword">Contraseña<a href="http://" target="_blank" rel="noopener noreferrer"></a></label>
-                                        <input type="password" class="form-control " id="inputPassword" required>
+                                        <input type="password" class="form-control " name="password" required>
                                     </div>
                                 </div>
                                 
                                 <div class="col-12 col-sm-12 col-md-6 col-lg-6  mb-0 mb-md-4 ">
                                     <div class="form-group">
                                         <label for="inputPassword">Confirma contraseña<a href="http://" target="_blank" rel="noopener noreferrer"></a></label>
-                                        <input type="password" class="form-control is-valid" id="inputPassword" required>
+                                        <input type="password" class="form-control is-valid" name="confirm-password" required>
                                     </div>
                                 </div>
-
-
+                                
+                                
                                 
                                 <div class="col-6 col-sm-6 col-md-6 col-lg-6  mb-0 mb-md-4 ">
                                     <div class="form-group">
                                         <label for="inputFechaNac">Fecha de nacimiento</label>
-                                        <input type="date" class="form-control" id="inputFechaNac" placeholder="Date of Birth" required>
+                                        <input type="date" class="form-control" name="birth" placeholder="Date of Birth" required>
                                     </div>
                                 </div>
                                 
                                 <div class="col-6 col-sm-6 col-md-6 col-lg-6  mb-0 mb-md-4">
                                     <label for="">Sexo</label>
-                                    <select class="custom-select">
-                                    <option selected>Seleccionar</option>
-                                    <option value="1">Hombre</option>
-                                    <option value="2">Mujer</option>
+                                    <select class="custom-select" name="sex">
+                                        <option selected>Seleccionar</option>
+                                        <option value="h">Hombre</option>
+                                        <option value="m">Mujer</option>
                                     </select>
-
+                                    
                                 </div>
+                                
+                                <div class="form-group">
+                                    <label for="exampleFormControlFile1">Ingrese imagen de perfil</label>
+                                    <input type="file" class="form-control-file" name="avatar">
+                                </div>
+                                
                                 
                                 
                                 <div class="col-6 col-sm-6 col-md-6 col-lg-9 mb-0 mb-md-4 ">
                                     <div class="form-check">
-                                        <input class="form-check-input" type="checkbox" value="" id="invalidCheck" required>
-                                        <label class="form-check-label" for="invalidCheck">
+                                        <input class="form-check-input" type="checkbox" value="" name="tyc_check" required>
+                                        <label class="form-check-label"  for="invalidCheck">
                                             Acepto los <a href="#" class="subrayado">términos y condiciones</a> y la <a href="#" class="subrayado">política de privacidad</a>.
                                         </label>
                                     </div>
                                 </div>
                                 
                                 <div class="col-6 col-sm-6 col-md-6 col-lg-3 ">
-                                <!--
-                                    <button class="btn btn-secondary float-right     mt-1" type="submit">Registrarse</button>
-                                -->
-                                        <button type="button" class="btn btn-secondary" data-toggle="modal" data-target="#exampleModal">
+                                    <!--
+                                        <button class="btn btn-secondary float-right     mt-1" type="submit">Registrarse</button>
+                                    -->
+                                    <button type="button" class="btn btn-secondary" data-toggle="modal" data-target="#exampleModal">
                                         Registrarme
-                                        </button>
-
-
-                                        <!-- Modal -->
-                                        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                    </button>
+                                    
+                                    
+                                    <!-- Modal -->
+                                    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
                                         <div class="modal-dialog modal-xl" role="document">
                                             <div class="modal-content text-center">
-                                            <div class="modal-header">
-                                                <h5 class="modal-title d-block color-verde w-100" id="exampleModalLabel">¡Ya casi estamos!</h5>
-                                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                                <span aria-hidden="true">&times;</span>
-                                                </button>
-                                            </div>
-                                            <div class="modal-body">
-                                                Elegí tus áreas favoritas para personalizar tu experiencia y ofrecerte desafíos acordes a tus intereses regularmente.
-
-                                                <div class="seleccion-intereses d-flex flex-column flex-md-row">
-    
-                                                    <input type="checkbox" id="myCheckbox1" value="Diseño y Arte" />
-                                                    <label for="myCheckbox1">
-                                                    <img src="img/categoria-diseno.jpg"><p class="mt-2">Diseño y Arte</p>
-                                                    </label>
-                                                    
-                                                    <input type="checkbox" id="myCheckbox2" value="Fotografía" />
-                                                    <label for="myCheckbox2">
-                                                    <img src="img/categoria-fotografia.jpg"><p class="mt-2">Fotografía</p>
-                                                    </label>
-                                                    
-                                                    <input type="checkbox" id="myCheckbox3" value="Programación y Lógica" />
-                                                    <label for="myCheckbox3">
-                                                    <img src="img/categoria-programacion.jpg"><p class="mt-2">Programación y Lógica</p>
-                                                    </label>
-
+                                                <div class="modal-header">
+                                                    <h5 class="modal-title d-block color-verde w-100" id="exampleModalLabel">¡Ya casi estamos!</h5>
+                                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                        <span aria-hidden="true">&times;</span>
+                                                    </button>
                                                 </div>
-
-
-                                            </div>
-                                            <div class="modal-footer mt-5">
-                                                <button type="button" class="btn btn-primary" data-dismiss="modal">Volver atrás</button>
-                                                <button class="btn btn-secondary float-right     mt-1" type="submit">Registrarme</button>
-                                            </div>
+                                                <div class="modal-body">
+                                                    Elegí tus áreas favoritas para personalizar tu experiencia y ofrecerte desafíos acordes a tus intereses regularmente.
+                                                    
+                                                    <div class="seleccion-intereses d-flex flex-column flex-md-row">
+                                                        
+                                                        <input type="checkbox" id="myCheckbox1" name="opcion_diseno_y_arte"  value="diseno_y_arte" />
+                                                        <label for="myCheckbox1">
+                                                            <img src="img/categoria-diseno.jpg"><p class="mt-2">Diseño y Arte</p>
+                                                        </label>
+                                                        
+                                                        <input type="checkbox" id="myCheckbox2" value="fotografía" name="opcion_fotografia"  />
+                                                        <label for="myCheckbox2">
+                                                            <img src="img/categoria-fotografia.jpg"><p class="mt-2">Fotografía</p>
+                                                        </label>
+                                                        
+                                                        <input type="checkbox" id="myCheckbox3" value="opcion_programación_y_lógica" />
+                                                        <label for="myCheckbox3">
+                                                            <img src="img/categoria-programacion.jpg"><p class="mt-2">Programación y Lógica</p>
+                                                        </label>
+                                                        
+                                                    </div>
+                                                    
+                                                    
+                                                </div>
+                                                <div class="modal-footer mt-5">
+                                                    <button type="button" class="btn btn-primary" data-dismiss="modal">Volver atrás</button>
+                                                    <button class="btn btn-secondary float-right     mt-1" type="submit">Registrarme</button>
+                                                </div>
                                             </div>
                                         </div>
-                                        </div>
-
+                                    </div>
+                                    
                                 </div>  
                             </form> 
                         </div>               
                     </div>
-                    
-                        
-                    </div>
-
-
-
-                </body>
+                </div>
+    </body>
+</html>     
